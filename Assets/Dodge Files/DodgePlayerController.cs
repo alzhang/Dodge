ec723e4337710f00ddce8fae300c81475b31276b
Alex Zhang <alzhang96@gmail.com>
2018-05-14 17:32:42 -0400

    Linked main game to minigame

---
+++ Assets/Dodge Files/DodgePlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgePlayerController : MonoBehaviour {
+
+
+    public float currentPosition = 0f;
+    public float speed = 0.075f;
+    public float radius = 465f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Input.GetAxis("Horizontal");
+        currentPosition += rotation*speed;
+
+        transform.position = new Vector3(Mathf.Cos(currentPosition)*radius, Mathf.Sin(currentPosition)*radius, 0);
+
+    }
+}
---
+++ Assets/Dodge Files/DodgeProjectileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeProjectileController : MonoBehaviour {
+
+
+    public GameObject beam;
+
+    public float frequency = .05f;
+
+	// Use this for initialization
+	void Start () {
+        int difficulty = MainGameStateController.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.value < frequency)
+        {
+            //spawn an indicator
+
+        }
+	}
+}
